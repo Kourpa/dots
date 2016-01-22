7e6fd411fbecc4c0ac2cbd94c01e10dfc03f3c24
Kourpa <mlemus01@unm.edu>
2016-01-22 00:19:15 -0700

    deleted everything

--- .config/chromium/Default/Extensions/gighmmpiobklfepjocnamgkkbiglidom/2.7.1_0/options/support.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $("#checkupdate").html(translate("checkforupdates"));
-  checkupdates("help");
-});
--- .config/chromium/Default/Extensions/hafdlehgocfcodbgjnpecfajgkeejnaa/1.1_0/js/devtools.js
+++
@@ -1,6 +0,0 @@
-chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(
-    function(resource, content) {
-        chrome.extension.sendMessage({event: "onResourceContentCommitted", resource : resource, content: content}, function(response) {
-          // nothing to be done
-        });
-    });
