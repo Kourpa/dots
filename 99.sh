fe6d9ae78a76fb1d124da4c9601f181622d78c0f
kourpa <mlemus01@unm.edu>
2017-09-05 22:17:41 -0600

    added awesome wm configuration

--- 99.sh
+++ 99.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #xdotool mousemove 561 257 click 3
-for i in `seq 1 1000`;
+for i in `seq 1 10`;
 do
     xdotool click 1 
-    sleep $((1+(1000 + RANDOM % 10000)/10000))
     sleep $(((7000 + RANDOM % 10000)/10000))
     sleep $(((7000 + RANDOM % 10000)/10000))
 done
