f4623f3f16b7e5cd6ef5cc72f7c369bb47d909f1
mlemus <mlemus@goraft.tech>
2023-02-07 16:32:59 -0600

    fixed config

--- config/nvim/lua/config/catpuccin.lua
+++ config/nvim/lua/config/catpuccin.lua
@@ -1,61 +1,49 @@
 local ok, _ = pcall(require, 'catppuccin')
 if ok then 
-    local catppuccin = require("catppuccin")
+    -- configure it
+    require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+            light = "latte",
+            dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+        term_colors = false,
+        dim_inactive = {
+            enabled = false,
+            shade = "dark",
+            percentage = 0.15,
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        styles = {
+            comments = { "italic" },
+            conditionals = { "italic" },
+            loops = {},
+            functions = {},
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            telescope = true,
+            notify = false,
+            mini = false,
+            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+    })
 
--- configure it
-    catppuccin.setup(
-    {
-            transparent_background = false,
-            term_colors = false,
-            styles = {
-                comments = "italic",
-                functions = "italic",
-                keywords = "italic",
-                strings = "NONE",
-                variables = "NONE",
-            },
-            integrations = {
-                treesitter = true,
-                native_lsp = {
-                    enabled = true,
-                    virtual_text = {
-                        errors = "italic",
-                        hints = "italic",
-                        warnings = "italic",
-                        information = "italic",
-                    },
-                    underlines = {
-                        errors = "underline",
-                        hints = "underline",
-                        warnings = "underline",
-                        information = "underline",
-                    },
-                },
-                lsp_trouble = false,
-                lsp_saga = false,
-                gitgutter = false,
-                gitsigns = true,
-                telescope = true,
-                nvimtree = {
-                    enabled = false,
-                    show_root = false,
-                },
-                which_key = false,
-                indent_blankline = {
-                    enabled = false,
-                    colored_indent_levels = false,
-                },
-                dashboard = false,
-                neogit = false,
-                vim_sneak = false,
-                fern = false,
-                barbar = false,
-                bufferline = false,
-                markdown = false,
-                lightspeed = false,
-                ts_rainbow = false,
-                hop = false,
-            },
-        }
-    )
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
 end
