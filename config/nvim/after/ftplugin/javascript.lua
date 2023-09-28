df0a5a83ea0fcf0fa1978ace7ac151110c66274a
mlemus <mlemus@goraft.tech>
2023-09-28 07:48:24 -0500

    updates

---
+++ config/nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,7 @@
+vim.cmd [[
+    set shiftwidth=2
+    set softtabstop=2
+    set tabstop=2
+    set shiftwidth=2
+    let g:test#javascript#runner = 'reactscripts'
+]]
---
+++ config/nvim/after/ftplugin/javascriptreact.lua
@@ -0,0 +1,8 @@
+vim.cmd [[
+    set shiftwidth=2
+    set softtabstop=2
+    set tabstop=2
+    set shiftwidth=2
+    let g:test#javascriptreact#runner = 'reactscripts'
+    let g:test#javascript#runner = 'reactscripts'
+]]
